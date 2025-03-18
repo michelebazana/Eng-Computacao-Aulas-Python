cf12ff7f7d37a6aaa5d37f634c0e0b2bde4c5bcc
michelebazana <michele.bazana@gmail.com>
2025-03-17 22:48:42 -0300

    Meu primeiro programa em Python.

--- primeiro_programa.py
+++ primeiro_programa.py
@@ -1,1 +1,29 @@
 print("Hello world!")
+print()
+
+# Exercício 1
+print("Michele\n")
+
+# Exercício 2
+a = 3
+b = 5
+print(2*a*3*b)
+print()
+# Exercício 3
+# num1 = 2
+# num2 = 3
+# num3 = 4
+# soma = num1+num2+num3
+# print(soma)
+
+# num1 = int(input("Número 1 = "))
+# num2 = int(input("Número 2 = "))
+# num3 = int(input("Número 3 = "))
+# soma = num1+num2+num3
+# print(soma)
+
+num1 = float(input("Número 1 = "))
+num2 = float(input("Número 2 = "))
+num3 = float(input("Número 3 = "))
+soma = num1+num2+num3
+print(f"Soma = {soma:.2f}")
