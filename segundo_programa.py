2a8e53bc3cb5a89eb2543f5a6c07ae0896e7194c
michelebazana <michele.bazana@gmail.com>
2025-03-24 22:49:54 -0300

    Segundo programa em Python.

---
+++ segundo_programa.py
@@ -0,0 +1,17 @@
+# d = True
+# print(type(d))
+#
+# nome = "Michele Bazana de Souza"
+# print(type(nome))
+#
+# print(10**3)
+# print(10//3)
+# print(10%3)
+
+nome_completo = input("Nome completo: ")
+idade = int(input("Idade: "))
+altura = float(input("Altura: "))
+
+print(f"Nome completo: {nome_completo}")
+print(f"Idade: {idade}")
+print("Altura:", altura)
