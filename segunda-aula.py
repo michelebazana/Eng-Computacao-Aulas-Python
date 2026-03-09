f2671e5540edfd7045038de26372c5ba69ac3c6e
michelebazana <michele.bazana@gmail.com>
2026-03-09 20:34:18 -0300

    Segunda aula.

---
+++ segunda-aula.py
@@ -0,0 +1,54 @@
+# meu primeira programa
+'''meu primeiro programa'''
+print("Hello World!")
+
+check1 = 8
+check2 = 5.5
+print(check1+check2)
+
+soma = check1+check2
+print(soma)
+
+media = (check1+check2)/2
+print(media)
+
+print(type(check1))
+print(type(check2))
+print(type(soma))
+print(type(media))
+
+nome = "Michele Bazana de Souza"
+print(type(nome))
+
+# Exercício 1
+print("Michele Bazana de Souza\n")
+
+# Exercício 2
+a = 3
+b = 5
+print(2*a*3*b,"\n")
+
+# Exercício 3
+check1 = 8
+check2 = 5.5
+check3 = 9
+print(check1+check2+check3)
+print()
+
+media = (check1+check2)/2
+print(f"Média = {media:.1f}")
+
+d = True
+print(type(d))
+
+resto = 9 % 3
+print(resto)
+
+inteira = 10 // 3
+print(inteira)
+print()
+
+check1 = float(input("Checkpoint 1 = "))
+check2 = float(input("Checkpoint 2 = "))
+media = (check1 + check2)/2
+print(f"Média = {media:.1f}")
